a635c21b14dbbdeec88b4206021424a49dec3d36
Shark1906 <amanraj1906@gmail.com>
2021-11-04 23:15:54 +0530

    MODIFIED : UC 2

--- src/com/emp/demo/Welcome.java
+++ src/com/emp/demo/Welcome.java
@@ -1,14 +1,21 @@
 package com.emp.demo;
 
 public class Welcome {
-
+	
+	public static final int IS_ABSENT = 0;
+	public static final int IS_PRESENT = 1;
+	public static final int WAGE_PER_HR =20;
+	public static final int FULL_TIME_HRS = 8;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation program");
 		
 		int random = (int)Math.floor(Math.random() * 10 % 2);
-		System.out.println(random);// 0 -> absent, 1-> present
-		
+	    if(random == IS_PRESENT) {
+	    	int empWage = FULL_TIME_HRS * WAGE_PER_HR;
+	    	System.out.println("Employee wage : "+empWage);	
+	    }
 	}
 
 }
