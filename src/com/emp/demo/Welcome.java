61a8bdd8dfd185ac0975dca3179c38ed1d469603
Shark1906 <amanraj1906@gmail.com>
2021-11-04 23:25:28 +0530

    MODIFIED : UC 3

--- src/com/emp/demo/Welcome.java
+++ src/com/emp/demo/Welcome.java
@@ -3,19 +3,27 @@ package com.emp.demo;
 public class Welcome {
 	
 	public static final int IS_ABSENT = 0;
-	public static final int IS_PRESENT = 1;
-	public static final int WAGE_PER_HR =20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HR = 20;
 	public static final int FULL_TIME_HRS = 8;
+	public static final int PART_TIME_HRS = 4;
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to employee wage computation program");
-		
-		int random = (int)Math.floor(Math.random() * 10 % 2);
-	    if(random == IS_PRESENT) {
-	    	int empWage = FULL_TIME_HRS * WAGE_PER_HR;
+		int empWage = 0;
+		int random = (int)Math.floor(Math.random() * 10 % 3);
+	    
+		if(random == IS_FULL_TIME) {
+	    	empWage = FULL_TIME_HRS * WAGE_PER_HR;
+	    	System.out.println("Employee wage : "+empWage);	
+	    }
+	    else if(random == IS_PART_TIME) {
+	    	empWage = PART_TIME_HRS * WAGE_PER_HR;
 	    	System.out.println("Employee wage : "+empWage);	
 	    }
 	}
-
 }
+
