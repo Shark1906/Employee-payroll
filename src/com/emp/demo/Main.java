3d2eb3292302594b9057404d8b0e0f2ac86d3965
Shark1906 <amanraj1906@gmail.com>
2021-11-12 21:35:56 +0530

    ADDED : UC 12

--- src/com/emp/demo/Main.java
+++ src/com/emp/demo/Main.java
@@ -7,13 +7,12 @@ public class Main {
 	System.out.println("Welcome to employee wage computation program");
 	EmpWageBuilder builder = new EmpWageBuilder();
 	CompanyEmpWage jio = new CompanyEmpWage(20, 20, 100, "Jio");
-	builder.calculateWage(jio);		
-	//System.out.println(jio);
-	builder.arrOfCompanies[0] = jio;
+	builder.calculateWage(jio);
+	builder.addCompanyEmpWage(jio);
 	CompanyEmpWage vi = new CompanyEmpWage(50, 30, 120, "Vi");
 	builder.calculateWage(vi);
-	//System.out.println(vi);
-	builder.arrOfCompanies[1] = vi;
-	System.out.println(builder.arrOfCompanies[0] + "\n" + builder.arrOfCompanies[1]);
+	builder.addCompanyEmpWage(vi);
+	builder.printAllCompanyEmpWage();
+	
 	}
 }
